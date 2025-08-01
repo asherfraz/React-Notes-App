9f4434a2a307f2e8a156e96f4cb3d170de434230
Asher Fraz <asherfraz@gmail.com>
2025-08-01 13:17:39 +0500

    FIX: Environment variables

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ const notesRoutes = require('./routes/notes.route');
 const { PORT, FRONTEND_URL } = require('./config/dotenvx');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const { Console } = require('console');
 
 
 // 3rd party middleware
@@ -18,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ["http://localhost:5173", FRONTEND_URL, FRONTEND_URL.slice(0, -1)],
+    origin: [FRONTEND_URL, FRONTEND_URL.slice(0, -1), "http://localhost:5173"],
     optionsSuccessStatus: 200,
     credentials: true,
 };
--- frontend/src/hook/useAuth.tsx
+++ frontend/src/hook/useAuth.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 
 export const useAuth = () => {
-	const user = useSelector((state: RootState) => state.user.user);
+	const user = useSelector((state) => state.user.user);
 	// console.log("User authentication status:", user);
 	return user;
 };
